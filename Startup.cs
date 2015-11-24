c7ef70be24ff8b6ee01e62b4ffd16b3b7bf0d986
gesturer-outlook <gesturer@outlook.com>
2015-11-24 18:22:15 +0530

    First checkin

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebappfromGITHub.Startup))]
+namespace WebappfromGITHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
